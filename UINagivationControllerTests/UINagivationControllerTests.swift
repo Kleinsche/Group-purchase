560e527583f89ff635a2aa2076ad59fda77d442f
沈志洋 <shenzhiyang163@163.com>
2016-07-20 19:18:48 +0800

    Initial Commit

---
+++ UINagivationControllerTests/UINagivationControllerTests.swift
@@ -0,0 +1,36 @@
+//
+//  UINagivationControllerTests.swift
+//  UINagivationControllerTests
+//
+//  Created by 🍋 on 16/7/20.
+//  Copyright © 2016年 🍋. All rights reserved.
+//
+
+import XCTest
+@testable import UINagivationController
+
+class UINagivationControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
